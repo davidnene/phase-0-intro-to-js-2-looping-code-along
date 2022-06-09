61ec434e2c08a47a32bfae01a9d3294a5cd21f41
davidnene <davienesh4@gmail.com>
2022-06-09 20:39:49 +0300

    commit tree c0f4fdd24f76384b7b0dd96e3cb69ccb7c00104c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+/*
+let spy;
+beforeEach( () => {
+    spy = sinon.spy( console, 'log' );
+  } );
+
+afterEach( () => {
+    spy.restore();
+  } );
+*/
+const friends = ['David','Paul','Ashley','Rachel'];
+
+  function writeCards(invites, quote) {
+      let newArray = [];
+      for (let i = 0; i < invites.length; i++) {
+          newArray.push(`Thank you, ${invites[i]}, for the wonderful ${quote} gift!`);         
+      };
+    return newArray;
+  };
+  console.log(writeCards(['p','o'],'suprise'));
+
+
+
+  function countDown(time) {
+      for(let seconds = time; seconds >=0; seconds--) {
+          console.log(seconds);
+      };
+  };
+
+  countDown(10);
